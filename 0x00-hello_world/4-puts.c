6fcfe0f87218058bae706388a8077fbcfd97f660
aremzo <iaremu@gmail.com>
2022-06-03 12:24:04 -0700

    r

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,17 +1,14 @@
 #include <stdio.h>
 
 /**
- * main - Entry point
- * My hello, printf task
- * Return: return 1 after program execution
+* main - Entry point
+* My hello, printf task
+* Return: return 1 after program execution
 */
 
 int main(void)
-
 {
 
-	 printf("Programming is like building a multilingual puzzle");
-
-	  return (0);
-
+printf("Programming is like building a multilingual puzzle");
+return (0);
 }
