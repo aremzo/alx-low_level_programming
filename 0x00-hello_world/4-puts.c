fc5df3863c0f046439c2ad737c7e36e3accae51d
aremzo <iaremu@gmail.com>
2022-06-03 05:06:01 -0700

    res

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,19 @@
 #include <stdio.h>
 
+/**
+ *
+ * * main-Entry point
+ *
+ * * Return: 0 if successful
+ *
+ * */
 
 int main(void)
+
 {
-	 puts("\"Programming is like building a multilingual puzzle");
-	  return (0);
+
+	puts("\"Programming is like building a multilingual puzzle");
+
+	return (0);
+
 }
