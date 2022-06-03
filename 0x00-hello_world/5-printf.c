2682e4e803eb77be5c81559baa693ebef5be163a
aremzo <iaremu@gmail.com>
2022-06-03 05:20:13 -0700

    issa

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,14 +1,10 @@
 #include <stdio.h>
 
 /**
- *
- *  * main - Entry point
- *
- *   * My hello, printf task
- *
- *    * Return: return 1 after program execution
- *
- *    */
+ * main - Entry point
+ * My hello, printf task
+ * Return: return 1 after program execution
+ */
 
 int main(void)
 
